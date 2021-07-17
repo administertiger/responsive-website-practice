0d37d690b3adf6c9102b0eb1c9b19ba81cce3c48
Tiger Surapat <tigersurapat@Tigers-MacBook-Pro.local>
2021-07-18 02:43:13 +0700

    done gallery

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log('test')
 
-let clickButton = document.querySelector('.h1');
-let container = document.querySelector('.container');
+let clickButton = document.querySelector('.sec1-h1');
+let container = document.querySelector('.sec1-container');
+let nameScroll = document.querySelector('.sec4-h1')
 
 clickButton.addEventListener('click', function () {
     container.classList.toggle('open');
     console.log('Click!!')
 });
+
 
